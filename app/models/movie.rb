98cf0b8124e6f077fe2a47ea8181f1bd949eecc3
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-08 11:26:42 -0700

    snapshot of master before changing rental model logic completely

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
 
   has_many :rentals
+  has_many :customers, through: :rentals
 
 	validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,26 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+
+  validates :checked_out, inclusion: { in: [true, false] },
+                          exclusion: { in: [nil] }
+
+  def self.check_out_date
+    return created_at.to_date
+  end
+
+  def self.due_date
+    return created_at.to_date.next_week
+  end
+
+  def self.overdue?
+    return checked_out && DateTime.now > due_date
+  end
+
+  def self.return_rental
+    raise ArgumentError.new("Not checked out") if !checked_out
+    checked_out = false
+    movie.available_inventory += 1
+  end
+
 end
