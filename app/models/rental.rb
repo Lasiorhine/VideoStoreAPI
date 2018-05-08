03f5ecc087c6bf1a4ae80cd6d8ed7957cc2fdc4d
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-08 12:06:19 -0700

    modified yml for rental model and laid out some model logic

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,25 +2,43 @@ class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
 
-  validates :checked_out, inclusion: { in: [true, false] },
-                          exclusion: { in: [nil] }
+  # after_commit :decrease_inventory, on: create
 
-  def self.check_out_date
-    return created_at.to_date
-  end
+  # First video
+  validates :valid_check_in_date
 
-  def self.due_date
-    return created_at.to_date.next_week
-  end
+  # validates :check_in_date, inclusion: { in: [true, false] },
+                          # exclusion: { in: [nil] }
 
-  def self.overdue?
-    return checked_out && DateTime.now > due_date
-  end
+  # def self.check_out_date
+  #   return created_at.to_date
+  # end
+  #
+  # def self.due_date
+  #   return created_at.to_date.next_week
+  # end
+  #
+  # def self.overdue?
+  #   return checked_out && DateTime.now > due_date
+  # end
+  #
+  # def self.return_rental
+  #   raise ArgumentError.new("Not checked out") if !checked_out
+  #   checked_out = false
+  #   movie.available_inventory += 1
+  # end
 
-  def self.return_rental
-    raise ArgumentError.new("Not checked out") if !checked_out
-    checked_out = false
-    movie.available_inventory += 1
-  end
+  private
+
+    # def self.decrease_inventory
+    #   movie.available_inventory += 1
+    # end
+
+
+    def valid_check_in_date
+      errors.add("Invalid check-in") if !check_in_date && check_in_date > Date.now
+      errors.add("Cannot change check-in date") if check_in_date
+    end
+
 
 end
